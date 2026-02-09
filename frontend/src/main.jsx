a2ef9c988993de776bbabd6298bc3d2b0a04d32d
Wise Munene Mutuku <wisemunene99@gmail.com>
2026-02-09 09:45:16 +0300

    home page working

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import { store } from "./app/store";
+import { store } from "./store/store";
 import { AuthProvider } from "./contexts/AuthContext";
 import App from "./App";
 import "./index.css";
